6e35bdd2703f9aea413c593138c8b1b7fc2af1e4
Greta Moseley <greta.moseley@gmail.com>
2017-12-20 15:45:30 -0600

    add C# exercise building 'likes' tracker with lists and list methods

--- CSharpUdemyPractice9/Program.cs
+++ CSharpUdemyPractice9/Program.cs
@@ -10,6 +10,58 @@ namespace CSharpUdemyPractice9
     {
         static void Main(string[] args)
         {
+            /*When you post a message on Facebook, depending on the number of people who like your post, 
+             * Facebook displays different information.
+
+                If no one likes your post, it doesn't display anything.
+                If only one person likes your post, it displays: [Friend's Name] likes your post.
+                If two people like your post, it displays: [Friend 1]
+            and[Friend 2] like your post.
+                If more than two people like your post, it displays: [Friend 1], [Friend 2] 
+                and[Number of Other People] others like your post.
+                Write a program and continuously ask the user to enter different names, until 
+                the user presses Enter (without supplying a name). Depending on the number of names provided, 
+                display a message based on the above pattern.
+            */
+
+            var nameInput = "";
+            var names = new List<string>();
+            var likes = 0;
+            
+            do
+            {   Console.WriteLine("Please enter a name: ");
+                nameInput = Console.ReadLine();
+                Console.WriteLine();//blank
+                //add each name to the list only if it's not there already, if it is a duplicate, don't add it.
+          
+                if (names.Contains(nameInput))
+                {
+                    Console.WriteLine("Sorry, no duplicate likes.");
+                }else if(!names.Contains(nameInput) && !String.IsNullOrWhiteSpace(nameInput)) //don't add blanks to the list either
+                {
+                    names.Add(nameInput);
+                }
+                
+                Console.WriteLine();//blank
+
+                //count determines path - if 1, if 2, if 3+
+                if (names.Count == 1)
+                {
+                    Console.WriteLine("{0} liked your post", names[0]);   
+
+                }else if (names.Count == 2)
+                {
+                    Console.WriteLine("{0} and {1} liked your post.", names[0], names[1]);
+                    
+                }
+                else if(names.Count >=3)//if 3+ likes
+                {
+                   likes = names.Count - 2;
+                  Console.WriteLine("{0} and {1} and {2} others liked your post.", names[0], names[1], likes);
+                }
+                Console.WriteLine();//blank
+
+            } while (String.IsNullOrWhiteSpace(nameInput) == false);
         }
     }
 }
